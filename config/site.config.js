a7a1cdd5cb9dda1b4a6eb2d7098ceea2959555ab
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2024-09-15 18:12:11 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Tetsuya Komuro OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Tetsuya Komuro/TK Music/Music/',
+  baseDirectory: '/Tetsuya Komuro/TK Music/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -38,11 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<p align="center"><a href="https://tkfans.pw" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
+    '<p align="center"><a href="https://tkfans.us" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Music', '/Music[FLAC]'],
+  protectedRoutes: ['/Music', '/Music FLAC'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:tkfanswind@gmail.com',
