fcd0e17129b71a394c47f858653e624aa188f8b2
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2025-05-19 11:14:54 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,14 +38,14 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    '<p align="center"><a href="https://www.tkfans.top" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/NEW', '/Video', '/Music'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:tkfanswind@gmail.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
@@ -55,6 +55,22 @@ module.exports = {
       name: 'GitHub',
       link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
+    {
+      name: 'YouTube',
+      link: 'https://www.youtube.com/c/tkfans1',
+    },
+    {
+      name: 'Telegram',
+      link: 'https://t.me/tkfans',
+    },
+    {
+      name: 'QQ',
+      link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jygQ60zAWgqe5ZQtODPkkO2-QLdDrfdd&authKey=4WMdr7%2BX6efBAQ6Twc6cBrQs6980dsb1eSXEK3QpAnSQWaL9GGHJuOvDgjXnNu6b&noverify=0&group_code=4097583',
+    },
+    {
+      name: 'itunes',
+      link: 'https://www.tkfans.top',
+    }
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
